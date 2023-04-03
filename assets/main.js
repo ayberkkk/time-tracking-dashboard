f222bd28b692a357775212f1061b2b9329778bbd
ayberkkk <ayberktiglii@gmail.com>
2023-04-03 17:48:31 +0300

    Initial Commit

---
+++ assets/main.js
@@ -0,0 +1,52 @@
+const daily = document.querySelector("#daily");
+const weekly = document.querySelector("#weekly");
+const monthly = document.querySelector("#monthly");
+const title = document.querySelectorAll(".title");
+const hours = document.querySelectorAll(".hour");
+const lastHour = document.querySelectorAll(".last-hour");
+
+const url = "/data.json";
+let finalData;
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => (finalData = data));
+
+window.addEventListener("load", () => {
+  daily.classList.add("active");
+  monthly.classList.remove("active");
+  weekly.classList.remove("active");
+  for (let i = 0; i <= 5; i++) {
+    title[i].innerHTML = finalData[i].title;
+    hours[i].innerHTML = `${finalData[i].timeframes.daily.current}hrs`;
+    lastHour[
+      i
+    ].innerHTML = `Last Day - ${finalData[i].timeframes.daily.previous}hrs`;
+  }
+});
+
+weekly.addEventListener("click", () => {
+  weekly.classList.toggle("active", true);
+  daily.classList.toggle("active", false);
+  monthly.classList.toggle("active", false);
+  for (let i = 0; i <= 5; i++) {
+    title[i].innerHTML = finalData[i].title;
+    hours[i].innerHTML = `${finalData[i].timeframes.weekly.current}hrs`;
+    lastHour[
+      i
+    ].innerHTML = `Last Week - ${finalData[i].timeframes.weekly.previous}hrs`;
+  }
+});
+
+monthly.addEventListener("click", () => {
+  monthly.classList.toggle("active", true);
+  daily.classList.toggle("active", false);
+  weekly.classList.toggle("active", false);
+  for (let i = 0; i <= 5; i++) {
+    title[i].innerHTML = finalData[i].title;
+    hours[i].innerHTML = `${finalData[i].timeframes.monthly.current}hrs`;
+    lastHour[
+      i
+    ].innerHTML = `Last Month - ${finalData[i].timeframes.monthly.previous}hrs`;
+  }
+});
